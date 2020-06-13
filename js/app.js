98a69701475da72b27dc2caf1904b4999c47fb9e
yash-93 <yashdeepbachhas93@gmail.com>
2020-06-14 02:21:28 +0530

    made responsive

--- js/app.js
+++ js/app.js
@@ -24,3 +24,39 @@ $("#contactus_link").click(function () {
     "slow"
   );
 });
+
+$("#about_linkf").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $("#about_content").offset().top - 95,
+    },
+    "slow"
+  );
+});
+
+$("#services_linkf").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $("#services").offset().top - 95,
+    },
+    "slow"
+  );
+});
+
+$("#contactus_linkf").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $("#contactus").offset().top - 50,
+    },
+    "slow"
+  );
+});
+
+$("#editingworksamples").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $("#ourwork").offset().top - 50,
+    },
+    "slow"
+  );
+});
