fc7d1929523dac6296585c3f80edb92653965c7a
yash-93 <yashdeepbachhas93@gmail.com>
2020-06-14 18:48:07 +0530

    scroll fix

--- js/app.js
+++ js/app.js
@@ -1,62 +1,82 @@
-$("#about_link").click(function () {
-  $("html,body").animate(
-    {
-      scrollTop: $("#about_content").offset().top - 95,
-    },
-    "slow"
-  );
-});
+// $("#about_link").click(function () {
+//   $("html,body").animate(
+//     {
+//       scrollTop: $("#about_content").offset().top - 95,
+//     },
+//     "slow"
+//   );
+// });
 
-$("#services_link").click(function () {
-  $("html,body").animate(
-    {
-      scrollTop: $("#services").offset().top - 95,
-    },
-    "slow"
-  );
-});
+// $("#services_link").click(function () {
+//   $("html,body").animate(
+//     {
+//       scrollTop: $("#services").offset().top - 95,
+//     },
+//     "slow"
+//   );
+// });
 
-$("#contactus_link").click(function () {
-  $("html,body").animate(
-    {
-      scrollTop: $("#contactus").offset().top - 50,
-    },
-    "slow"
-  );
-});
+// $("#contactus_link").click(function () {
+//   $("html,body").animate(
+//     {
+//       scrollTop: $("#contactus").offset().top - 50,
+//     },
+//     "slow"
+//   );
+// });
 
-$("#about_linkf").click(function () {
-  $("html,body").animate(
-    {
-      scrollTop: $("#about_content").offset().top - 95,
-    },
-    "slow"
-  );
-});
+// $("#about_linkf").click(function () {
+//   $("html,body").animate(
+//     {
+//       scrollTop: $("#about_content").offset().top - 95,
+//     },
+//     "slow"
+//   );
+// });
 
-$("#services_linkf").click(function () {
-  $("html,body").animate(
-    {
-      scrollTop: $("#services").offset().top - 95,
-    },
-    "slow"
-  );
-});
+// $("#services_linkf").click(function () {
+//   $("html,body").animate(
+//     {
+//       scrollTop: $("#services").offset().top - 95,
+//     },
+//     "slow"
+//   );
+// });
 
-$("#contactus_linkf").click(function () {
-  $("html,body").animate(
-    {
-      scrollTop: $("#contactus").offset().top - 50,
-    },
-    "slow"
-  );
-});
+// $("#contactus_linkf").click(function () {
+//   $("html,body").animate(
+//     {
+//       scrollTop: $("#contactus").offset().top - 50,
+//     },
+//     "slow"
+//   );
+// });
 
-$("#editingworksamples").click(function () {
-  $("html,body").animate(
-    {
-      scrollTop: $("#ourwork").offset().top - 50,
-    },
-    "slow"
-  );
-});
+// $("#editingworksamples").click(function () {
+//   $("html,body").animate(
+//     {
+//       scrollTop: $("#ourwork").offset().top - 50,
+//     },
+//     "slow"
+//   );
+// });
+
+function scrollToAbout() {
+  var element = document.querySelector("#about_content");
+  element.scrollIntoView({ behavior: "smooth", block: "end", block: "center" });
+}
+
+function scrollToServices() {
+  var element = document.querySelector("#services");
+  element.scrollIntoView({ behavior: "smooth", block: "end", block: "center" });
+}
+
+function scrollToContact() {
+  var element = document.querySelector("#contactus");
+  element.scrollIntoView({ behavior: "smooth", block: "end", block: "center" });
+}
+
+function scrollToEditingSamples() {
+  var element = document.querySelector("#ourwork");
+  element.scrollIntoView({ behavior: "smooth", block: "end", block: "center" });
+}
